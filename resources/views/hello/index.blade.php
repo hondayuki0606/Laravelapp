d5370dc66d4900a2c01fa5e8dbc02f56cbf06272
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-11-11 23:43:55 +0900

    $loopによるループ変数 $loop->first $loop->iteration $loop->last

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -11,15 +11,15 @@ h1{ font-size:100px; }
 <h1>Index/blade</h1>
 <p>&#064;foreachディレクティブの例</p>
 <ol>
-@for ($i=1;$i<100;$i++)
-@if($i % 2 == 1)
-    @continue
-@elseif($i <= 10)
-<li>No,{{$i}}
-@else
-    @break
+@foreach ($data as $item)
+@if($loop->first)
+    <p>※データ一覧</p>
 @endif
-@endfor
+<li>No,{{$loop->iteration}}. {{$item}}</li>
+@if($loop->last)
+</ul><p>---ここまで</p>
+@endif
+@endforeach
 </ol>
 </body>
 </html>
