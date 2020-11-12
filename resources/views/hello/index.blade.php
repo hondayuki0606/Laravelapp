662bbacf762572a172535355c429b485cd50f1e5
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-11-12 22:31:11 +0900

    @whileのディレクティブについて @phpのディレクトリについて

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,24 +2,17 @@
 <html>
 <head>
 <title>Hello/Index</title>
-<style>
-body{ font-size:16px; }
-h1{ font-size:100px; }
-</style>
-</head>
-<body>
-<h1>Index/blade</h1>
-<p>&#064;foreachディレクティブの例</p>
+<p>&#064;whiteのディレクティブ例</p>
 <ol>
-@foreach ($data as $item)
-@if($loop->first)
-    <p>※データ一覧</p>
-@endif
-<li>No,{{$loop->iteration}}. {{$item}}</li>
-@if($loop->last)
-</ul><p>---ここまで</p>
-@endif
-@endforeach
+    @php
+    $counter = 0;
+    @endphp
+    @while ($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+    @php
+    $counter++;
+    @endphp
+    @endwhile
 </ol>
 </body>
 </html>
